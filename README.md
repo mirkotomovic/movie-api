2140d5c3889cca39bcbcaf6e57649d96025846a3
mirkotomovic <mirko.mire.tomovic@gmail.com>
2020-11-26 07:57:06 +0100

    About and readme page change

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ App is built on top of free public api [TVMaze](http://www.tvmaze.com/api).
 Icon pack used [React icons](https://www.npmjs.com/package/react-icons)
 
 SVG images from [undraw](https://undraw.co)
+
+Project demo on [netlify](https://tvmaze-api-mirko.netlify.app)
--- src/components/About.js
+++ src/components/About.js
@@ -4,11 +4,14 @@ const About = () => {
   return (
     <div className="about">
       <h1>About page</h1>
-      <p>This website is made using <a href="https://www.tvmaze.com/api">TVMaze api</a>.</p>
-      <p>It is a react project hosted on netify.</p>
-      <p>Application is bootstraped with <a href="https://create-react-app.dev/">Create-react-app</a>.</p>
-      <p>Icon pack used <a href="https://www.npmjs.com/package/react-icons">React icons</a>.</p>
-      <p>SVG images from <a href="https://undraw.co">undraw</a>.</p>
+      <ul>
+        <li>This website is made using <a href="https://www.tvmaze.com/api">TVMaze api</a>.</li>
+        <li>It is a react project hosted on netify.</li>
+        <li>Application is bootstraped with <a href="https://create-react-app.dev/">Create-react-app</a>.</li>
+        <li>Icon pack used <a href="https://www.npmjs.com/package/react-icons">React icons</a>.</li>
+        <li>SVG images from <a href="https://undraw.co">undraw</a>.</li>
+        <li>Project github repo <a href="https://github.com/mirkotomovic/movie-api">link</a>.</li>
+      </ul>
     </div>
   )
 }
