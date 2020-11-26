8fe51e28092a44114a019ae52d2c9ee1c73d2f8a
mirkotomovic <mirko.mire.tomovic@gmail.com>
2020-11-26 07:49:10 +0100

    added about page

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="about">
+      <h1>About page</h1>
+      <p>This website is made using <a href="https://www.tvmaze.com/api">TVMaze api</a>.</p>
+      <p>It is a react project hosted on netify.</p>
+      <p>Application is bootstraped with <a href="https://create-react-app.dev/">Create-react-app</a>.</p>
+      <p>Icon pack used <a href="https://www.npmjs.com/package/react-icons">React icons</a>.</p>
+      <p>SVG images from <a href="https://undraw.co">undraw</a>.</p>
+    </div>
+  )
+}
+
+export default About
